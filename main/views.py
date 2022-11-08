88318900dd566bcd155413e33fb5820f4e73ce1b
Diego Saavedra <dsaavedra88@gmail.com>
2022-11-07 21:59:16 -0500

    Ejemplo Cursos Django

---
+++ main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Curso
+
+def homepage(request):
+    #return HttpResponse("Hola esto es Django desde la App")
+    return render(request, "main/inicio.html",{"cursos":Curso.objects.all})
+
