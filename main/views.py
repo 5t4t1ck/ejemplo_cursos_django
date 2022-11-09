8fef26970b72127bf4bc211eca2933728077ca80
Diego Saavedra <dsaavedra88@gmail.com>
2022-11-08 21:15:41 -0500

    plantillas, materialize

--- main/views.py
+++ main/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 from .models import Curso
 
 def homepage(request):
     #return HttpResponse("Hola esto es Django desde la App")
     return render(request, "main/inicio.html",{"cursos":Curso.objects.all})
-
